5ceb75fd12a6671fbc18d46d40bf56627061aec6
saqibjaved0322 <105146668+saqibjaved0322@users.noreply.github.com>
2024-11-28 13:06:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+SESSION_ID: process.env.SESSION_ID || "X2RADDxS#dqSFzmEyAtKZ-XtFMCugaMYomGSGTMjXvuNL5uLqS-0",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SILENT-SOBX-MD WHATSAPP BOT 😊♻️",
+ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SAQI-JD WHATSAPP BOT 😊♻️",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
@@ -20,6 +20,6 @@ FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
 HEART_REACT: process.env.HEART_REACT || "true",
 OWNER_REACT: process.env.OWNER_REACT || "true",
-BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+BOT_NAME: process.env.BOT_NAME || "➺MIRSA🎉<",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "39c18b31", // omdbapi.com
 };
